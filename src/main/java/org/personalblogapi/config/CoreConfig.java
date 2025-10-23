500e025a810242fb5d3b2c68f0cf731bc6c29a03
plongrotha <Mrr.rothabetta31@gmail.com>
2025-10-23 16:23:42 +0700

    add

--- src/main/java/org/personalblogapi/config/CoreConfig.java
+++ src/main/java/org/personalblogapi/config/CoreConfig.java
@@ -14,7 +14,7 @@ public class CoreConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOriginPatterns("*")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTION")
                 .allowedHeaders("*")
                 .allowCredentials(true)
                 .maxAge(3600); // Cache preflight response for 1 hour
--- src/main/java/org/personalblogapi/controller/AuthController.java
+++ src/main/java/org/personalblogapi/controller/AuthController.java
@@ -15,14 +15,12 @@ import org.personalblogapi.service.AuthService;
 import org.personalblogapi.utils.ResponseUtil;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/auth")
 @RequiredArgsConstructor
+@CrossOrigin
 public class AuthController {
 
     private final AuthService authService;
